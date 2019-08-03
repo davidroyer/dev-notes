6962271823f13bdd4436929d90e12573635da3e7
David Royer <droyer01@gmail.com>
2019-08-03 02:09:26 -0400

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,44 +1,44 @@
 module.exports = {
   head: [
     [
-      'link',
+      "link",
       {
-        rel: 'stylesheet',
-        href: 'https://use.fontawesome.com/releases/v5.0.13/css/all.css'
+        rel: "stylesheet",
+        href: "https://use.fontawesome.com/releases/v5.0.13/css/all.css"
         // integrity: 'sha384-xxx',
         // crossorigin: 'anonymous'
       }
     ]
-  ],  
+  ],
   title: "DevNotes",
   description: "Developer Notes for David Royer",
   markdown: {
     plugins: [
-      'fontawesome', // equals to @org/markdown-it-foo if exists
+      "fontawesome" // equals to @org/markdown-it-foo if exists
       // 'fontawesome', // equals to @org/markdown-it-foo if exists
       // ['markdown-it-bar', {
       //   // provide options here
       // }]
     ],
     extendMarkdown: md => {
-      md.set({ breaks: true })
-      md.use(require('markdown-it-mermaid').default);
+      md.set({ breaks: true });
+      md.use(require("markdown-it-mermaid").default);
       // md.use(require('markdown-it-xxx'))
     }
-  },  
+  },
   themeConfig: {
     sidebar: [
-    "/",
-    "/circleci.md",
-    "/git.md",
-    "/javascript.md",
-    "/releases.md",
-    "/vscode.md",
-    "/vue-plugins.md",
-    "/vue-testing.md",
-    "/vue2-editor.md",
-    "/vuepress.md"
-    "/firebase.md"
-  ]
+      "/",
+      "/circleci.md",
+      "/git.md",
+      "/javascript.md",
+      "/releases.md",
+      "/vscode.md",
+      "/vue-plugins.md",
+      "/vue-testing.md",
+      "/vue2-editor.md",
+      "/vuepress.md",
+      "/firebase.md"
+    ]
   }
 };
